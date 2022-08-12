725996bc0dd0f519a247e3fdaa4e913988bad01a
AdeevMardia2008 <71126451+AdeevMardia2008@users.noreply.github.com>
2022-08-12 20:44:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const io = require("socket.io")(server, {
     }
 });
 
-const {ExpressPeerServer}=require("peer")
-const peerServer=ExpressPeerServer(server,{
-    debug:true
-})
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
 
-app.use("/peerjs",peerServer)
+app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
@@ -28,13 +28,12 @@ app.get("/:room", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-    socket.on("join-room",(roomId,userId,username)=>{
-        socket.join(roomId)
-    
-    socket.on("message", (message) => {
-        io.to(roomId).emit("createMessage", message,username);
-    })
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
     });
 });
 
-server.listen(process.env.PORT || 3030);
+server.listen(3030);
